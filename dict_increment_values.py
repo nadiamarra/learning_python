10abbc287be4bc3ffab7c6508fa6218fb69a1de5
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-02 13:59:23 +0100

    Update dict_increment_values.py

--- dict_increment_values.py
+++ dict_increment_values.py
@@ -5,6 +5,12 @@ def increment_values(d):
     '''
 
     for k in d:
-        d[k]=d[k]+1
+        d[k]=d[k]+1      
+ 
 
+#we pass the KEY to get the VALUE
+#ex. 
+#>>>d={'a':4,'b':8,'c':12}
+#>>>d['b']
+#>>>8
     
