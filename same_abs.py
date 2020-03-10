e4ab6fc3590a42c6dbb45b154ecf05d513187f8f
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-10 15:16:47 +0100

    Add files via upload

---
+++ same_abs.py
@@ -0,0 +1,19 @@
+def same_abs(num1, num2):
+    ''' (number, number) -> bool
+    Return True if and only if num1 and num2 have the same absolute value.
+    >>> same_abs(12.5, -12.5)
+    True
+    >>> same_abs(3, 4.5)
+    False
+    '''
+
+    return abs(num1)==abs(num2)
+
+
+
+
+    
+##    if abs(num1) == abs(num2):
+##        return True
+##    else:
+##        return False
