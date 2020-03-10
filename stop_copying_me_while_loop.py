8a7f3878718b64b0d9fa78e2ceb515630a8debc3
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-10 15:18:41 +0100

    Add files via upload

---
+++ stop_copying_me_while_loop.py
@@ -0,0 +1,9 @@
+#ask for input
+#user enters a response
+#we get the same response printed
+
+msg=input("Enter a sentence: ")
+while msg!=("Stop copying me"):
+    print(msg)
+    msg=input("Enter a sentence: ")
+print("Ugh fine you win")
