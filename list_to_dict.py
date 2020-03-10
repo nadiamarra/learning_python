b0506f24f1bfb5c87219f7cbff45980feea2ec24
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-10 15:02:24 +0100

    Add files via upload

---
+++ list_to_dict.py
@@ -0,0 +1,12 @@
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+answer = {}
+
+##for item in person:
+##    answer={item[0]:item[1]}
+
+
+##answer={item[0]:item[1] for item in person}
+
+
+for item in person:
+    answer={item[0]:item[1]}
