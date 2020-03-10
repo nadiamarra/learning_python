32b72b436c7737cb24ddf59669026ff43b609496
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-10 15:07:21 +0100

    Add files via upload

---
+++ make_pairs.py
@@ -0,0 +1,18 @@
+def make_pairs(list1, list2):
+    ''' (list of str, list of int) -> list of [str, int] list
+
+    Return a new list in which each item is a 2-item list with
+    the string from the corresponding position of list1 and the int from the corresponding position of list2.
+
+    Precondition: len(list1) == len(list2)
+
+    >>> make_pairs(['A', 'B', 'C'], [1, 2, 3])
+    [['A', 1], ['B', 2], ['C', 3]]
+    '''
+
+    pairs = []
+
+    for i in range(len(list1)):
+        pairs.append([list1[i],list2[i]])
+
+    return pairs
