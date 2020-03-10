344303826b7f0c7d50b240a2a54b5c721c790ec2
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-03-10 15:17:24 +0100

    Add files via upload

---
+++ scream_with_break.py
@@ -0,0 +1,11 @@
+times=input("How many times do I have to tell you? ")
+times=int(times)
+
+for scream in range(times):
+    print("CLEAN UP YOUR ROOM!")
+    if scream>=3:
+        print("do you even listen anymore?")
+        break
+
+    
+    
