63b2738c8118ebfc2fc2ad31179fbd0330837cec
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-04-02 16:31:42 +0200

    Add files via upload

---
+++ truthy_values.py
@@ -0,0 +1,13 @@
+def compact(lista):
+
+
+    '''(list)->list
+
+    Return a list of truthy values given a list with truthy and falsey values.
+
+    compact([0,1,2,"",[], False, {}, None, "All done"]) # [1,2, "All done"]
+    '''
+
+
+    truthy_list=[val for val in lista if bool(val) is True]
+    return truthy_list
