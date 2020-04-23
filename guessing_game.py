5dacbdba518aa21a463e2bc12ac7d2dc3fe03d8c
nadjmental <61318975+nadjmental@users.noreply.github.com>
2020-04-23 18:28:09 +0200

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,18 +2,22 @@ import random
 random_number=random.randint(1,10)
     
 while True:
-    msg=input("Guess a number from 1 to 10: ")
-    msg=int(msg)
-    if msg==random_number:
-        print("You won!")
-        play_again=input("Play again? (y/n) ")
-        if play_again=="y":
-            random_number=random.randint(1,10)
-            msg=""
-        else:
-            print("Thanks, bye!")
-            break
-    elif msg>random_number:
-        print("Too high! Guess again")
+    try:
+        msg=int(input("Guess a number from 1 to 10: "))
+    except ValueError:
+        print("That's not a number!")
     else:
-        print("Too low! Guess again")     
+        if msg==random_number:
+            print("You won!")
+            play_again=input("Play again? (y/n) ")
+            if play_again=="y":
+                random_number=random.randint(1,10)
+                msg=""
+            else:
+                print("Thanks, bye!")
+                break
+        elif msg>random_number:
+            print("Too high! Guess again")
+        else:
+            print("Too low! Guess again")     
+    
