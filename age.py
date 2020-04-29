b205c092ba8a91487346734269ee041760ad093b
nadiamarra <61318975+nadiamarra@users.noreply.github.com>
2020-04-29 14:41:23 +0200

    Add files via upload

---
+++ age.py
@@ -0,0 +1,7 @@
+name=input("What's your name? ")
+age=int(input("What year where you born? "))
+num=int(input("How many times do you want to read my message? "))
+hundred=100+age
+print(f"{name}, you will be 100 in the year {hundred}\n"*num)
+
+
